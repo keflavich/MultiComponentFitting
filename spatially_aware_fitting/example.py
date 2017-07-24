b4875335bbe1a9b1b2fa900a967f6c27d925ef8d
e-koch <koch.eric.w@gmail.com>
2017-07-24 16:21:09 -0600

    Up the number of points in the example; wider lines; TIL lmfit has emcee built-in

--- spatially_aware_fitting/example.py
+++ spatially_aware_fitting/example.py
@@ -9,10 +9,12 @@ imp.reload(minicube_fit)
 
 from minicube_fit import minicube_model, unconstrained_fitter
 
-model = minicube_model(np.arange(10),
+num_pts = 100
+
+model = minicube_model(np.arange(num_pts),
                        1, 0.5, -0.1,
-                       5, 1, -2,
-                       1, 0.1, 0.01,)
+                       25, 1, -2,
+                       10, 0.1, 0.01,)
 
 model_with_noise = np.random.randn(*model.shape)*0.1 + model
 
@@ -20,13 +22,22 @@ guess = {'amp': 0.8, 'ampdx': 0, 'ampdy': 0,
          'center': 5, 'centerdx': 0, 'centerdy': 0,
          'sigma': 1.5, 'sigmadx': 0, 'sigmady': 0,}
 
-result = unconstrained_fitter(model_with_noise, np.arange(10), guess)
-print("Result = {0}".format(result.params))
+result = unconstrained_fitter(model_with_noise, np.arange(num_pts), guess)
+for par in result.params:
+    print("{0}: {1}+/-{2}".format(par, result.params[par].value,
+                                  result.params[par].stderr))
+
+# Now use emcee to fit
+result_mcmc = result.emcee(steps=1000, burn=500)
+for par in result_mcmc.params:
+    print("{0}: {1}+/-{2}".format(par, result_mcmc.params[par].value,
+                                  result_mcmc.params[par].stderr))
 
 # plot
 import pylab as pl
 
-fitcube = minicube_model(np.arange(10), *[x.value for x in result.params.values()])
+fitcube = minicube_model(np.arange(num_pts), *[x.value for x in result.params.values()])
+fitcube_mcmc = minicube_model(np.arange(num_pts), *[x.value for x in result_mcmc.params.values()])
 
 pl.figure(1).clf()
 for ii,(yy,xx) in enumerate(np.ndindex((3,3))):
@@ -36,6 +47,8 @@ for ii,(yy,xx) in enumerate(np.ndindex((3,3))):
     ax.plot(model_with_noise[:,yy,xx], 'k-', zorder=-5, linewidth=1,
             drawstyle='steps-mid')
     ax.plot(fitcube[:,yy,xx], 'b--', zorder=0, linewidth=1,
+            drawstyle='steps-mid')
+    ax.plot(fitcube_mcmc[:,yy,xx], 'g--', zorder=0, linewidth=1,
             drawstyle='steps-mid')
     ax.plot(model_with_noise[:,yy,xx] - fitcube[:,yy,xx], 'r:', zorder=-1, linewidth=1,
             drawstyle='steps-mid')
