e9e96927eadafeb2022b58c618246324620a107b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-07-24 14:52:53 -0600

    make the example nicer

--- spatially_aware_fitting/example.py
+++ spatially_aware_fitting/example.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 
-# HACK
-import imp
-import minicube_fit
-imp.reload(minicube_fit)
+# # HACK
+# import imp
+# import minicube_fit
+# imp.reload(minicube_fit)
 
 from minicube_fit import minicube_model, unconstrained_fitter
 
@@ -14,11 +14,11 @@ model = minicube_model(np.arange(10),
                        5, 1, -2,
                        1, 0.1, 0.01,)
 
-model_with_noise = np.random.randn(*model.shape) + model
+model_with_noise = np.random.randn(*model.shape)*0.1 + model
 
 guess = {'amp': 0.8, 'ampdx': 0, 'ampdy': 0,
          'center': 5, 'centerdx': 0, 'centerdy': 0,
          'sigma': 1.5, 'sigmadx': 0, 'sigmady': 0,}
 
 result = unconstrained_fitter(model_with_noise, np.arange(10), guess)
-print("Result = {0}".format(result))
+print("Result = {0}".format(result.params))
