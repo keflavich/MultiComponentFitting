a54e70c6fb1e9dc1ebe870c24a3aafeafed2d364
e-koch <koch.eric.w@gmail.com>
2017-07-25 17:55:42 -0600

    Model w/ component selection

--- mcmcmc/mcmcmc.py
+++ mcmcmc/mcmcmc.py
@@ -1,4 +1,5 @@
 import pymc3 as pm
+import theano.tensor as TT
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,6 +9,7 @@ def BayesianMultiComponentFit(Xdata, Ydata, y_error=None, max_ncomp=3):
     """
 
     model = pm.Model()
+
     if max_ncomp == 1:
         with model:
             amp1 = pm.Uniform('amp1', lower=0, upper=1.2 * Ydata.max())
@@ -91,6 +93,77 @@ def BayesianMultiComponentFit(Xdata, Ydata, y_error=None, max_ncomp=3):
         trace = pm.sample()
 
     return(trace, model)
+
+
+def theano_gaussian(x, on, amp, vcen, sigv):
+    return on * amp * TT.exp(- 0.5 * TT.power((x - vcen) / sigv, 2))
+
+
+def MultiComponentFit(Xdata, Ydata, y_error=None, max_ncomp=3, min_p=0.5,
+                      **fit_kwargs):
+    '''
+    Try choosing the number of components as its own variable.
+    '''
+
+    ncomp = max_ncomp
+
+    while ncomp > 0:
+        model = pm.Model()
+
+        param_dict = {}
+
+        with model:
+
+            for i in np.arange(ncomp):
+                ii = i + 1
+                param_dict['amp{}'.format(ii)] = \
+                    pm.Uniform('amp{}'.format(ii), lower=0,
+                               upper=1.2 * Ydata.max())
+                param_dict['vcen{}'.format(ii)] = \
+                    pm.Uniform('vcen{}'.format(ii), lower=Xdata.min(),
+                               upper=Xdata.max())
+                param_dict['sigv{}'.format(ii)] = \
+                    pm.Uniform('sigv{}'.format(ii), lower=1e-5,
+                               upper=np.ptp(Xdata / 2.))
+                param_dict["on{}".format(ii)] = \
+                    pm.Bernoulli('on{}'.format(ii), p=0.5)
+
+                if i == 0:
+                    mu = theano_gaussian(Xdata,
+                                         param_dict['on{}'.format(ii)],
+                                         param_dict['amp{}'.format(ii)],
+                                         param_dict['vcen{}'.format(ii)],
+                                         param_dict['sigv{}'.format(ii)])
+                else:
+                    mu += theano_gaussian(Xdata,
+                                          param_dict['on{}'.format(ii)],
+                                          param_dict['amp{}'.format(ii)],
+                                          param_dict['vcen{}'.format(ii)],
+                                          param_dict['sigv{}'.format(ii)])
+
+            sigma = pm.InverseGamma('sigma', alpha=1, beta=y_error)
+            Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=Ydata)
+
+            start = pm.find_MAP()
+
+            trace = pm.sample(start=start, **fit_kwargs)
+
+        # Get the samples for the on parameters
+        on_frac = np.zeros((ncomp, ))
+        for i in np.arange(ncomp):
+            samps = trace.get_values('on{}'.format(i + 1))
+            on_frac[i] = samps.sum() / float(samps.shape[0])
+
+        print(on_frac)
+
+        if any(on_frac < min_p):
+            ncomp -= 1
+            continue
+        else:
+            break
+
+    return trace, model
+
 
 def RealizedFit(trace, XdataIn, model, n_draws=10, with_noise=False):
     Xdata = np.expand_dims(XdataIn, axis=1)
