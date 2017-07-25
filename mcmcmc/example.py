731e40bfa08036a3f249d5e0b330afc1a8a08836
e-koch <koch.eric.w@gmail.com>
2017-07-25 15:27:43 -0600

    Keep the clean components so we can compare in  the plot

--- mcmcmc/example.py
+++ mcmcmc/example.py
@@ -6,10 +6,17 @@ import matplotlib.pyplot as plt
 
 nPts = 401
 Xdata = np.linspace(-5, 5, nPts)
-Ydata = 5.2 * np.exp(-(Xdata - 0.3)**2 / (2*0.4**2)) + np.random.randn(nPts)
-Ydata += 1.3 * np.exp(-(Xdata +0.4)**2 / (2*1.0**2))
+Ydata_comp1 = 5.2 * np.exp(-(Xdata - 0.3)**2 / (2*0.4**2))
+Ydata_comp2 = 2.0 * np.exp(-(Xdata + 0.4)**2 / (2*1.0**2))
+Ydata_orig = Ydata_comp1 + Ydata_comp2
 sigma = 1.0
+Ydata = Ydata_orig + sigma * np.random.randn(nPts)
+
 plt.plot(Xdata, Ydata)
-trace, model = mcmcmc.BayesianMultiComponentFit(Xdata, Ydata, max_ncomp=3, y_error = 1.0)
+plt.plot(Xdata, Ydata_comp1, 'k:', linewidth=3, alpha=0.5)
+plt.plot(Xdata, Ydata_comp2, 'k:', linewidth=3, alpha=0.5)
+plt.plot(Xdata, Ydata_orig, 'k-.', linewidth=5, alpha=0.5)
+trace, model = mcmcmc.BayesianMultiComponentFit(Xdata, Ydata, max_ncomp=3,
+                                                y_error=sigma)
 Ymodels = mcmcmc.RealizedFit(trace, Xdata, model)
-plt.plot(Xdata, Ymodels, color='red')
+plt.plot(Xdata, Ymodels, color='red', alpha=0.5)
