bc87245e2ab6dc2135472c19658bfd22e297831c
e-koch <koch.eric.w@gmail.com>
2017-07-25 13:04:01 -0600

    Add pymc fitting to the example

--- spatially_aware_fitting/example.py
+++ spatially_aware_fitting/example.py
@@ -8,9 +8,10 @@ import minicube_fit
 imp.reload(minicube_fit)
 
 from minicube_fit import minicube_model, unconstrained_fitter
+from minicube_pymc import minicube_pymc_fit
 
 num_pts = 100
-npix = 3
+npix = 9
 
 model = minicube_model(np.arange(num_pts),
                        0.2, 0.5, -0.1,
@@ -22,9 +23,9 @@ model[model<0] = 0
 
 model_with_noise = np.random.randn(*model.shape)*0.1 + model
 
-guess = {'amp': 0.8, 'ampdx': 0, 'ampdy': 0,
-         'center': 5, 'centerdx': 0, 'centerdy': 0,
-         'sigma': 1.5, 'sigmadx': 0, 'sigmady': 0,}
+guess = {'amp': 0.3, 'ampdx': 0, 'ampdy': 0,
+         'center': 25, 'centerdx': 0, 'centerdy': 0,
+         'sigma': 10, 'sigmadx': 0, 'sigmady': 0,}
 
 result = unconstrained_fitter(model_with_noise, np.arange(num_pts), guess,
                               npix=npix)
@@ -40,6 +41,14 @@ for par in result_mcmc.params:
     print("{0}: {1}+/-{2}".format(par, result_mcmc.params[par].value,
                                   result_mcmc.params[par].stderr))
 
+# MCMC fit w/ pymc3
+pymc_medians, pymc_stddevs, trace, model = \
+    minicube_pymc_fit(np.arange(num_pts), model_with_noise, guess)
+print("pymc Parameters:")
+for par in pymc_medians:
+    print("{0}: {1}+/-{2}".format(par, pymc_medians[par],
+                                  pymc_stddevs[par]))
+
 # plot
 import pylab as pl
 
@@ -48,6 +57,10 @@ fitcube = minicube_model(np.arange(num_pts),
                          npix=npix)
 fitcube_mcmc = minicube_model(np.arange(num_pts),
                               *[x.value for x in result_mcmc.params.values()],
+                              npix=npix)
+
+fitcube_pymc = minicube_model(np.arange(num_pts),
+                              *[pymc_medians[x] for x in pymc_medians if x is not 'on'],
                               npix=npix)
 
 
@@ -62,6 +75,8 @@ for ii,((yy,xx), ax) in enumerate(zip(np.ndindex((npix,npix)), axes.ravel())):
     ax.plot(fitcube[:,yy,xx], 'b--', zorder=0, linewidth=1,
             drawstyle='steps-mid')
     ax.plot(fitcube_mcmc[:,yy,xx], 'g--', zorder=0, linewidth=1,
+            drawstyle='steps-mid')
+    ax.plot(fitcube_pymc[:,yy,xx], 'r-.', zorder=0, linewidth=1,
             drawstyle='steps-mid')
     ax.plot(model_with_noise[:,yy,xx] - fitcube[:,yy,xx], 'r:', zorder=-1, linewidth=1,
             drawstyle='steps-mid')
